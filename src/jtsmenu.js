2886048ce3fb74cda2db93f7c4d3179ba5938dbb
botzzz3 <77111954+botzzz3@users.noreply.github.com>
2021-02-24 09:48:39 -0300

    Add files via upload

---
+++ src/jtsmenu.js
@@ -0,0 +1,26 @@
+const jtsmenu = (prefix, sender) => {
+	return `ola,voce esta no menu jts :)
+
+SALVE✌️
+◪ *INFORMAÇÕES*
+    ❏ nome do bot: @jts_ofc
+    ❏ 
+    ❏
+ ࿇ ══━━━━✥◈✥━━━━══ ࿇
+    use sempre ! para cada comando 
+    ❏ sticker( converte imagens,gif em                 figurinhas)
+    ❏ menuadmin
+    ❏ tts( de um /tts para saber as línguas dele)
+    ❏ 
+    ❏
+    ❏
+    ❏
+    ❏'
+}
+
+exports.jtsmenu = jtsmenu
+
+
+
+
+
